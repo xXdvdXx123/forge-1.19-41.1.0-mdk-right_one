5292430b98921f891e3158e69c1ee9ad80341847
BuildTools <unconfigured@null.spigotmc.org>
2022-11-09 15:26:43 +0100

    Added important comment

--- src/main/java/net/dvdx/tutorialmod/TutorialMod.java
+++ src/main/java/net/dvdx/tutorialmod/TutorialMod.java
@@ -17,6 +17,7 @@ public class TutorialMod
     public static final String MOD_ID = "tutorialmod";
     private static final Logger LOGGER = LogUtils.getLogger();
 
+    // important sosso
     public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
