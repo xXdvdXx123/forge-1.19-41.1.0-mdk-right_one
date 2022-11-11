c64d5f52c6c9acf54a975ed85f344bb287c77605
BuildTools <unconfigured@null.spigotmc.org>
2022-11-11 20:01:56 +0100

    Custom Key Bind

---
+++ src/main/java/net/dvdx/tutorialmod/event/ClientEvents.java
@@ -0,0 +1,31 @@
+package net.dvdx.tutorialmod.event;
+
+import net.dvdx.tutorialmod.TutorialMod;
+import net.dvdx.tutorialmod.util.KeyBinding;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+public class ClientEvents {
+    @Mod.EventBusSubscriber(modid = TutorialMod.MOD_ID, value = Dist.CLIENT)
+    public static class ClientForgeEvents {
+        @SubscribeEvent
+        public static void onKeyInput(InputEvent.Key event) {
+            if(KeyBinding.DRINKING_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Pressed the drinking key!"));
+            }
+        }
+    }
+    @Mod.EventBusSubscriber(modid = TutorialMod.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModBusEvents {
+        @SubscribeEvent
+        public static void onKeyRegister(RegisterKeyMappingsEvent event) {
+            event.register(KeyBinding.DRINKING_KEY);
+        }
+    }
+
+}
